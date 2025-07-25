43a766b135f005bb9f257596fc2e8b9f0161b8d3
Yen-Ting Liu <ytliu2@illinois.edu>
2025-07-24 23:33:29 -0500

    Add user generator

--- schemas/generate_users.sql
+++ schemas/generate_users.sql
@@ -1,9 +1,8 @@
--- GCP MySQL maximum lines for information_schema is 4284 lines
-INSERT INTO User (username)
-SELECT CONCAT('user', LPAD(n, 6, '0'))
-FROM (
-  SELECT @rownum := @rownum + 1 AS n
-  FROM information_schema.columns, (SELECT @rownum := 0) r
-  LIMIT 1000
-) x
-ORDER BY RAND();
+INSERT INTO MoodLog (user_id, song_id, mood_label, rating, ts)
+SELECT  (SELECT user_id FROM User ORDER BY RAND() LIMIT 1),
+        (SELECT song_id FROM Song ORDER BY RAND() LIMIT 1),
+        ELT(FLOOR(1+RAND()*5),'Happy','Sad','Calm','Energetic','Angry'),
+        FLOOR(60+RAND()*40),
+        NOW() - INTERVAL FLOOR(RAND()*90) DAY
+FROM    information_schema.columns
+LIMIT   100;
