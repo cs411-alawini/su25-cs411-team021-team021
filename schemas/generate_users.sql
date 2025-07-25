cfc401b278666799ffadef5d5e3697fa976feba8
Yen-Ting Liu <ytliu2@illinois.edu>
2025-07-24 23:37:54 -0500

    Add back deleted user generator

--- schemas/generate_users.sql
+++ schemas/generate_users.sql
@@ -1,3 +1,13 @@
+-- GCP MySQL maximum lines for information_schema is 4284 lines
+INSERT INTO User (username)
+SELECT CONCAT('user', LPAD(n, 6, '0'))
+FROM (
+  SELECT @rownum := @rownum + 1 AS n
+  FROM information_schema.columns, (SELECT @rownum := 0) r
+  LIMIT 1000
+) x
+
+-- insert mood logs
 INSERT INTO MoodLog (user_id, song_id, mood_label, rating, ts)
 SELECT  u.user_id,
         s.song_id,
