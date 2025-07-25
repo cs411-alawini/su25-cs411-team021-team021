0114a1d79923ae20e1230c255a6497e31b5dcf3d
Yen-Ting Liu <ytliu2@illinois.edu>
2025-07-24 23:35:49 -0500

    Faster generate user.

--- schemas/generate_users.sql
+++ schemas/generate_users.sql
@@ -1,8 +1,12 @@
 INSERT INTO MoodLog (user_id, song_id, mood_label, rating, ts)
-SELECT  (SELECT user_id FROM User ORDER BY RAND() LIMIT 1),
-        (SELECT song_id FROM Song ORDER BY RAND() LIMIT 1),
+SELECT  u.user_id,
+        s.song_id,
         ELT(FLOOR(1+RAND()*5),'Happy','Sad','Calm','Energetic','Angry'),
         FLOOR(60+RAND()*40),
         NOW() - INTERVAL FLOOR(RAND()*90) DAY
-FROM    information_schema.columns
-LIMIT   100;
+FROM
+  (SELECT user_id FROM User ORDER BY RAND() LIMIT 1000) u
+CROSS JOIN
+  (SELECT song_id FROM Song ORDER BY RAND() LIMIT 1000) s
+ORDER BY RAND()
+LIMIT 5000;
